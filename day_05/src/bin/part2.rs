6c86a46c01e5fb979e8c49cbb4a390c2672d3d8d
Myrtus <myrtus0x0@protonmail.com>
2023-12-05 14:59:39 -0800

    optimized day 5

--- day_05/src/bin/part2.rs
+++ day_05/src/bin/part2.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use itertools::Itertools;
 
 #[derive(Copy, Debug, Clone)]
@@ -8,6 +7,7 @@ struct Map {
     range_length: u64,
 }
 
+#[derive(Debug, Clone)]
 struct GardenMap {
     translation_maps: Vec<Map>
 }
@@ -34,69 +34,112 @@ fn parse_map(map_data:&str) -> GardenMap {
     return gard_map
 }
 
+fn translate_map_to_seed(garden_map_slice: &[GardenMap]) -> Vec<u64> {
+    let mut results = Vec::new();
+    // we only care about the last map
+    let focus_map = garden_map_slice.iter().nth(garden_map_slice.len() -1).unwrap();
+    
+    // for each later in the last map, we need to find the valid candidates
+    for f_layer in &focus_map.translation_maps {
+        
+        // we only need to prcess the start and end of the src range
+        let tries = vec![f_layer.src_range_start, f_layer.src_range_start+f_layer.range_length];
+        for candidate in tries {
+            let mut rev_traversal = candidate;
+            
+            // reverse traverse our map translating the src input to the initial seed
+            for (_i, map) in garden_map_slice.iter().rev().enumerate() {
+                for layer in &map.translation_maps {
+                    if  rev_traversal >= layer.dest_range_start && rev_traversal < layer.dest_range_start + layer.range_length {
+                        rev_traversal = layer.src_range_start + (rev_traversal - layer.dest_range_start);
+                        break;
+                    }
+                }
+            }
+            
+            if !results.contains(&rev_traversal) {
+                results.push(rev_traversal);
+            }
+        }
+    }
+
+    return results;
+}
+
+fn reverse_traverse_maps(seed_info: Vec<u64>, maps:&Vec<GardenMap>) -> Vec<u64> {
+    let mut res = Vec::new();
+    for (i, _map) in maps.iter().enumerate() {
+        // we can take the lowest value here in the translation layers and walk backwards in the maps to get a seed
+        // if the seed is in our initial set then we are going to process it 
+        let seeds = translate_map_to_seed(&maps[0..i+1]);
+        for seed in seeds {
+            for info in seed_info.chunks(2) {
+                let start = *info.iter().nth(0).unwrap();
+                let range = *info.iter().nth(1).unwrap();
+                // if the seeds we generated from filtering are in the 
+                // inital list of seeds then we append
+                if seed >= start && seed <= start + range {
+                    res.push(seed);
+                } 
+            }
+        }      
+    }
+
+    return res;
+}
+
+fn traverse_maps(seed:u64, maps:&Vec<GardenMap>) -> u64 {
+    let mut traversal: u64 = seed;
+
+    for map in maps {        
+        for layout_range in &map.translation_maps {
+            if  traversal >= layout_range.src_range_start && 
+                traversal < layout_range.src_range_start + layout_range.range_length {
+                    traversal = layout_range.dest_range_start + (traversal - layout_range.src_range_start);
+                    break
+            }
+        }
+    }
+
+    return traversal;
+}
+
+fn gather_seed_info(chunk:&str) -> Vec<u64> {
+    let mut seeds = Vec::new();
+    let seed_info = chunk.split(": ").nth(1).unwrap().split(" ").map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+    for mut temp_chunk in &seed_info.iter().chunks(2) {
+        let start = temp_chunk.next().unwrap();
+        let range = temp_chunk.next().unwrap();
+        seeds.push(*start as u64);
+        seeds.push(*range as u64);
+    }
+
+    return seeds;
+}
+
 fn part2(seed_map:&str) -> u64 {
     let mut maps: Vec<GardenMap> = Vec::new();
-    let mut seeds: Vec<u64> = Vec::new();
+    let mut seed_info: Vec<u64> = Vec::new();
     for (i, chunk) in seed_map.split("\n\n").enumerate() {
         // parse out the seeds
         if i == 0 {
-            let seed_info = chunk.split(": ").nth(1).unwrap().split(" ").map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-            for mut temp_chunk in &seed_info.iter().chunks(2) {
-                let start = temp_chunk.next().unwrap();
-                let range = temp_chunk.next().unwrap();
-                println!("pushing seeds: {} - {}", start, start+range);
-                for j in 0..*range {
-                    seeds.push((start+j) as u64);
-                }
-            }
+            seed_info = gather_seed_info(chunk);
         } else {
             maps.push(parse_map(chunk));
         }
     }
 
+    let pre_filtered_seeds:u64 = seed_info.iter().step_by(2).sum();
+    dbg!(pre_filtered_seeds);
+    let filtered_seeds = reverse_traverse_maps(seed_info, &maps);
+    dbg!(&filtered_seeds.len());
+
     let mut final_destinations: Vec<u64> = Vec::new();
-    for seed in seeds {
-        let mut traversal: u64 = seed;
-        let mut travel_str: String = String::new();
-        travel_str.push_str(&traversal.to_string());
-        
-        for map in &maps {
-            let old_trav = traversal;
-            
-            for layout_range in &map.translation_maps {
-                let smax = layout_range.src_range_start + layout_range.range_length - 1;
-                if smax < traversal {
-                    continue;
-                }
-
-                // if layout_range.src_range_start > rmax || traversal > rmax {
-                //     break;
-                // }
-                if traversal >= layout_range.src_range_start && traversal <= layout_range.src_range_start + layout_range.range_length {
-                    let mut diff: u64 = 0;
-                    if layout_range.dest_range_start > layout_range.src_range_start {
-                        diff = layout_range.dest_range_start - layout_range.src_range_start;
-                        traversal = traversal + diff;
-                    } else {
-                        diff = layout_range.src_range_start - layout_range.dest_range_start;
-                        traversal = traversal - diff;
-                    }
-                    
-                    travel_str.push_str("->");
-                    travel_str.push_str(&traversal.to_string());
-                    break
-                }
-            }
-            if traversal == old_trav {
-                travel_str.push_str("->");
-                travel_str.push_str(&traversal.to_string());
-            }
-        }
-
+    for seed in filtered_seeds {
+        let traversal = traverse_maps(seed, &maps);
         final_destinations.push(traversal);
-        println!("seed: {} = {}", seed, travel_str);
     }
-
+    
     return *final_destinations.iter().min().unwrap();
 }
 
@@ -146,5 +189,6 @@ humidity-to-location map:
 60 56 37
 56 93 4");
         assert_eq!(result, 46);
+        // assert_eq!(part2(include_str!("../puzzle")), 6472060);
     }
 }
